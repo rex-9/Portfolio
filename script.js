4d03052902b963f29bc0a59c2ce21905743ea74c
Htet Naing <htetnaing0814@gmail.com>
2022-06-14 01:13:33 +0630

    Portfolio details popup windows for mobile and desktop

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Menu
 const sideNav = document.querySelector(".sideNav")
 const ham = document.querySelector(".ham")
 const menuX = document.querySelector(".menuX")
@@ -12,4 +13,25 @@ menuX.addEventListener("click", toggleHamburger)
 
 function toggleHamburger() {
     sideNav.classList.toggle("showNav")
+}
+
+// Popup
+var project = document.getElementById("project");
+var btns = document.querySelectorAll('[id=project-btn]');;
+var close = document.getElementsByClassName("close")[0];
+
+btns.forEach (btn => {
+    btn.addEventListener("click", function () {
+        project.style.display = "block";
+    })
+})
+
+close.onclick = function () {
+    project.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == project) {
+        project.style.display = "none";
+    }
 }
