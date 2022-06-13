32f6b0f4adbc495a6b17cecbc0622ae3f470a3c5
Htet Naing <htetnaing0814@gmail.com>
2022-06-14 01:47:44 +0630

    added eslint and fixed linters errors

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 // Menu
-const sideNav = document.querySelector(".sideNav")
-const ham = document.querySelector(".ham")
-const menuX = document.querySelector(".menuX")
-const menuItems = document.querySelectorAll(".menuLink")
-
-menuItems.forEach(menuItem => {
-    menuItem.addEventListener("click", toggleHamburger)
-})
-
-ham.addEventListener("click", toggleHamburger)
-menuX.addEventListener("click", toggleHamburger)
+const sideNav = document.querySelector('.sideNav');
+const ham = document.querySelector('.ham');
+const menuX = document.querySelector('.menuX');
+const menuItems = document.querySelectorAll('.menuLink');
 
 function toggleHamburger() {
-    sideNav.classList.toggle("showNav")
+  sideNav.classList.toggle('showNav');
 }
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', toggleHamburger);
+});
+
+ham.addEventListener('click', toggleHamburger);
+menuX.addEventListener('click', toggleHamburger);
 
 // Popup
-var project = document.getElementById("project");
-var btns = document.querySelectorAll('[id=project-btn]');;
-var close = document.getElementsByClassName("close")[0];
+const project = document.getElementById('project');
+const btns = document.getElementsByClassName('project-btn');
+const close = document.getElementsByClassName('close')[0];
 
-btns.forEach (btn => {
-    btn.addEventListener("click", function () {
-        project.style.display = "block";
-    })
-})
+for (let i = 0; i < btns.length; i += 1) {
+  btns[i].onclick = function () {
+    project.style.display = 'block';
+  };
+}
 
 close.onclick = function () {
-    project.style.display = "none";
-}
+  project.style.display = 'none';
+};
 
 window.onclick = function (event) {
-    if (event.target == project) {
-        project.style.display = "none";
-    }
-}
+  if (event.target === project) {
+    project.style.display = 'none';
+  }
+};
