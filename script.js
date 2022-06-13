4726959f34813ed1ee474ff41186d0b7eb4aec79
Htet Naing <htetnaing0814@gmail.com>
2022-06-14 02:15:30 +0630

    stored project information to local storage

--- script.js
+++ script.js
@@ -16,6 +16,18 @@ ham.addEventListener('click', toggleHamburger);
 menuX.addEventListener('click', toggleHamburger);
 
 // Popup
+
+const myProject = {
+  name: 'Keeping track of hundreds of components',
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+  featuredImage: './assets/project-detail-desktop.png',
+  technologies: ['Codekit', 'GitHub', 'Javascript', 'Bootstrap', 'Terminal', 'Codepen'],
+  liveLink: 'https://htetnaing0814.github.io/Portfolio/',
+  sourceLink: 'https://github.com/HtetNaing0814/Portfolio',
+};
+
+window.localStorage.setItem('myObject', JSON.stringify(myProject));
+
 const project = document.getElementById('project');
 const btns = document.getElementsByClassName('project-btn');
 const close = document.getElementsByClassName('close')[0];
