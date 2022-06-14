f3ccc1200cf53bef1778612b10505d272679569e
Htet Naing <htetnaing0814@gmail.com>
2022-06-14 22:27:15 +0630

    Validate contact form for email

--- script.js
+++ script.js
@@ -73,3 +73,61 @@ window.onclick = function (event) {
     project.style.display = 'none';
   }
 };
+
+// Validate contact form
+
+function showMessage(input, message, type) {
+	const msg = input.parentNode.querySelector("p");
+	msg.innerText = message;
+  if (type === true) {
+    input.classList.add('success')
+  } else {
+    input.classList.add('error')
+  }
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email) || email != email.toLowerCase()) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.querySelector("#signup");
+
+const NAME_REQUIRED = "Please enter your name";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter a correct email address format using only lowercase letters";
+
+form.addEventListener("submit", function (event) {
+	event.preventDefault();
+
+	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+	if (nameValid && emailValid) {
+		alert("Demo only. No form was posted.");
+	}
+});
